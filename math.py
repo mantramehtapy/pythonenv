1f6b64fe60938ceb088f440461da1fe322ed57ed
Mantra.Mehta <84002990+mantramehtapy@users.noreply.github.com>
2024-09-10 15:50:06 +0000

    new

---
+++ math.py
@@ -0,0 +1,29 @@
+
+def plus(a,b):
+    try:
+        print(a+b)
+    except:
+        print("error")
+
+def subtract(a,b):
+    try:
+    print(a-b)
+except:
+    print("error")
+
+def div(a,b):
+    try:
+    print(a%b)
+except:
+    print("error")
+
+def multi(a,b):
+    try:
+    print(a*b)
+except:
+    print("error")
+
+plus(1,2)
+subtract(56,8)
+div(21123,2134)
+multi(12,6)
